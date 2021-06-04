805b606b4c564adc9f99ea6ff98168502986ba5e
Ana Ceprnja <aceprnja@iun.edu>
2021-06-03 18:28:53 -0600

    problem three in java

--- coding_challenge.java
+++ coding_challenge.java
@@ -18,3 +18,29 @@ public class Solution {
         }
     }
 }
+
+// Problem three
+// https://www.hackerrank.com/challenges/30-arrays/problem
+
+	public class Solution {
+
+    	public static void main(String[] args) {
+        	// Get the input
+        	Scanner sc = new Scanner(System.in);
+        	int length = sc.nextInt();
+        	int[] array = new int[length];
+        
+       		for (int i = 0; i < length; i++) {
+            	array[i] = sc.nextInt();
+        	}
+        
+        	// Var holding our new string
+        	String result = "";
+        
+        	for (int i = array.length - 1; i >= 0; i--) {
+            	result = result + array[i] + " ";
+        	}
+        
+        	System.out.println(result);
+    	}
+	}
