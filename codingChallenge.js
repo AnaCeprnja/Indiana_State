7398a855bbe344d91b7b42490b4e017fdea3caf9
Ana Ceprnja <aceprnja@iun.edu>
2021-06-03 18:14:54 -0600

    problem one solved in javascript

---
+++ codingChallenge.js
@@ -0,0 +1,8 @@
+/*problem one
+https://www.hackerrank.com/challenges/30-hello-world/problem */
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    // Write the second line of output that prints the contents of 'inputString' here.
+    console.log(inputString);
+}
